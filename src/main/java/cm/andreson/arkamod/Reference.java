8cbe75a5888c9fcb05bf5dab2acda1c1f81873e8
'Nicolas <'nicolas.thauvin@co-assist.fr'>
2020-12-28 21:28:12 +0100

    test

--- src/main/java/cm/andreson/arkamod/Reference.java
+++ src/main/java/cm/andreson/arkamod/Reference.java
@@ -4,7 +4,7 @@ package cm.andreson.arkamod;
 public class Reference {
 	
 	public static final String MOD_ID = "arkamod";
-	public static final String MOD_NAME = "Arka mod";
+	public static final String MOD_NAME = "Arka mod";//test
 	public static final String version = "1.0.0";
 	public static final String CLIENT_PROXY = "cm.andreson.arkamod.proxy.clientproxy";
 	public static final String SERVEUR_PROXY = "cm.andreson.arkamod.proxy.CommonProxy";
