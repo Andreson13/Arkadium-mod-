c5052a9f1e4920f69503c333e7e8b9c15123d59c
Andreson13 <75949994+Andreson13@users.noreply.github.com>
2021-02-24 23:14:42 +0100

    Outild Armures Crafts

--- src/main/java/cm/andreson/arkamod/craft/CraftCreation.java
+++ src/main/java/cm/andreson/arkamod/craft/CraftCreation.java
@@ -3,6 +3,7 @@ package cm.andreson.arkamod.craft;
 import cm.andreson.arkamod.blocks.BlockCreation;
 import cm.andreson.arkamod.items.ItemCreation;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
 public class CraftCreation {
@@ -38,18 +39,66 @@ public class CraftCreation {
 		//Item_test
 		GameRegistry.addRecipe(new ItemStack(ItemCreation.Item_test, 9), new Object[]
 				{"#",
-				 '#', BlockCreation.arkadium_block });
+				 '#', BlockCreation.arkadium_block, 
+				 'X', Items.stick});
 		
 		/**************/
 		/*CRAFT ARMURE*/
 		/**************/
-		
+		GameRegistry.addRecipe(new ItemStack(ItemCreation.casqueArmor, 1), new Object[]
+				{"###",
+				 "# #",
+				 '#', ItemCreation.Item_test });
+		GameRegistry.addRecipe(new ItemStack(ItemCreation.plastronArmor, 1), new Object[]
+				{"# #",
+				 "###",
+				 "###",
+				 '#', ItemCreation.Item_test });
+		GameRegistry.addRecipe(new ItemStack(ItemCreation.pentalonArmor, 1), new Object[]
+				{"###",
+				 "# #",
+				 "# #",
+				 '#', ItemCreation.Item_test });
+		GameRegistry.addRecipe(new ItemStack(ItemCreation.bootsArmor, 1), new Object[]
+				{"   ",
+				 "# #",
+				 "# #",
+				 '#', ItemCreation.Item_test });
 		
 		
 		/**************/
 		/*CRAFT OUTILS*/
 		/**************/
-	  
+		GameRegistry.addRecipe(new ItemStack(ItemCreation.ARKADIUM_PICKAXE, 1), new Object[]
+				{"###",
+				 " X ",
+				 " X ",
+				 '#', ItemCreation.Item_test,  
+				 'X', Items.stick});
+		GameRegistry.addRecipe(new ItemStack(ItemCreation.ARKADIUM_HOE, 1), new Object[]
+				{"##",
+				 " X",
+				 " X",
+				 '#', ItemCreation.Item_test, 
+				 'X', Items.stick});
+		GameRegistry.addRecipe(new ItemStack(ItemCreation.ARKADIUM_SPADE, 1), new Object[]
+				{" #",
+				 " X",
+				 " X",
+				 '#', ItemCreation.Item_test, 
+				 'X', Items.stick});
+		GameRegistry.addRecipe(new ItemStack(ItemCreation.ARKADIUM_SWORD, 1), new Object[]
+				{" #",
+				 " #",
+				 " X",
+				 '#', ItemCreation.Item_test, 
+				 'X', Items.stick});
+		GameRegistry.addRecipe(new ItemStack(ItemCreation.ARKADIUM_AXE, 1), new Object[]
+				{"##",
+				 "#X",
+				 " X",
+				 '#', ItemCreation.Item_test, 
+				 'X', Items.stick});
 		
 		
 		/*************/
